3006eacf0f04ef3ff09a7a703f4dfe6de55f1a42
Florent Callaou <florent.callaou22@gmail.com>
2017-10-02 11:43:21 +0200

    01-default-method

--- src/test/java/java8/ex01/Method_01_Test.java
+++ src/test/java/java8/ex01/Method_01_Test.java
@@ -13,55 +13,66 @@ import java8.data.Person;
  */
 public class Method_01_Test {
 
-    // tag::IDao[]
-    interface IDao {
-        List<Person> findAll();
+	// tag::IDao[]
+	interface IDao {
+		List<Person> findAll();
 
-        // TODO créer une méthode int sumAge()
-        // TODO Cette méthode retourne le résultat de l'addition des ages des personnes
-    }
-    // end::IDao[]
 
-    class DaoA implements IDao {
+		// TODO créer une méthode int sumAge()
+		// TODO Cette méthode retourne le résultat de l'addition des ages des personnes
+		default int sumAge(){
+			int sumAge = 0;
+			for (Person person : findAll()) {
+				sumAge += person.getAge();
+			}
+			return sumAge;
 
-        List<Person> people = Data.buildPersonList(20);
+		}
+	}
+	// end::IDao[]
 
-        @Override
-        public List<Person> findAll() {
-            return people;
-        }
-    }
 
-    class DaoB implements IDao {
 
-        List<Person> people = Data.buildPersonList(100);
+	class DaoA implements IDao {
 
-        @Override
-        public List<Person> findAll() {
-            return people;
-        }
-    }
+		List<Person> people = Data.buildPersonList(20);
 
-    @Test
-    public void test_daoA_sumAge() throws Exception {
+		@Override
+		public List<Person> findAll() {
+			return people;
+		}
+	}
 
-        DaoA daoA = new DaoA();
+	class DaoB implements IDao {
 
-        // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+		List<Person> people = Data.buildPersonList(100);
 
-        assert result == 210;
-    }
+		@Override
+		public List<Person> findAll() {
+			return people;
+		}
+	}
 
-    @Test
-    public void test_daoB_sumAge() throws Exception {
+	@Test
+	public void test_daoA_sumAge() throws Exception {
 
-        DaoB daoB = new DaoB();
+		DaoA daoA = new DaoA();
 
-        // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+		// TODO invoquer la méthode sumAge pour que le test soit passant
+		int result = daoA.sumAge();
 
-        assert result == 5050;
+		assert result == 210;
+	}
 
-    }
+	@Test
+	public void test_daoB_sumAge() throws Exception {
+
+		DaoB daoB = new DaoB();
+
+		// TODO invoquer la méthode sumAge pour que le test soit passant
+		int result = daoB.sumAge();;
+
+		assert result == 5050;
+
+	}
 }
--- src/test/java/java8/ex02/Method_02_Test.java
+++ src/test/java/java8/ex02/Method_02_Test.java
@@ -16,9 +16,13 @@ public class Method_02_Test {
     interface IDao {
         List<Person> findAll();
 
+       
         // TODO créer une méthode String format()
         // TODO la méthode retourne une chaîne de la forme [<nb_personnes> persons]
-        // TODO exemple de résultat : "[14 persons]", "[30 persons]"
+        // TODO exemple de résultat : "[14 persons]", "[30 persons]" 
+        default String format(){
+        	return "[" + findAll().size() + " persons]"; 
+        }
     }
     // end::IDao[]
 
@@ -30,6 +34,10 @@ public class Method_02_Test {
         @Override
         public List<Person> findAll() {
             return people;
+        }
+        
+        public String format(){
+        	return this.getClass().toString() + IDao.super.format();
         }
 
         // TODO redéfinir la méthode String format()
@@ -46,7 +54,7 @@ public class Method_02_Test {
         DaoA daoA = new DaoA();
 
         // TODO invoquer la méthode format() pour que le test soit passant
-        String result = null;
+        String result = daoA.format();
 
         "DaoA[20 persons]".equals(result);
     }
